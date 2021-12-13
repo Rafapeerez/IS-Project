b85166488ad8f000b9706f77292b210d2ebc79e3
Antonio Jiménez <antoniojj1712@gmail.com>
2021-12-13 18:56:30 +0100

    Maquinas

---
+++ Maquinas.cc
@@ -0,0 +1,30 @@
+#include "maquina.h"
+using namespace std;
+
+ bool setnumMaquinas(int nMaquinas){
+     if(nMaquinas>0 && nMaquinas<=8){
+         nMaquinas_=nMaquinas;
+         return true;
+     }
+     exit (0);
+     return false;
+ }
+
+
+ void darAlta_Baja_Maquina(int maquinas){
+     fstream f("Maquinas.txt", fstream::app);
+    if(!f){
+        cout<<"Ha ocurrido un error al intentar abrir el fichero 'Maquinas.txt' \n";
+        exit(EXIT_FAILURE);
+    }
+    int numMaquinas;
+    cout<<"Introduzca el numero de maquinas disponibles: "<<endl;
+    for(int i=0;i<nMaquinas_;i++){
+        cin>>numMaquinas;
+        setnumMaquinas(numMaquinas);
+        f<<getnumMaquinas()<<endl;
+        
+    }
+
+    f.close();
+ }
