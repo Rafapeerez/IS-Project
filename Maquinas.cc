db327ba856d6b8886c6459f0ca2647f91cc296ae
Antonio Jiménez <antoniojj1712@gmail.com>
2021-12-16 19:01:30 +0100

    Maquinas

--- Maquinas.cc
+++ Maquinas.cc
@@ -1,26 +1,25 @@
 #include "maquinas.h"
+#include <fstream>
 using namespace std;
 
- bool Maquina::setnumMaquinas(int nMaquinas){
-     if(nMaquinas_>0 && nMaquinas<=8){
+bool Maquina::setnumMaquinas(int nMaquinas){
+    if(nMaquinas_>0 && nMaquinas<=8){
         nMaquinas_=nMaquinas;
         return true;
-     }
-     exit(0);
-     return false;
- }
+    }
+    return false;
+}
 
 bool Maquina::setMaquinasDisponibles(int identMaq){
-     if(identMaq>0 && identMaq<=8){
+    if(identMaq>0 && identMaq<=8){
         identMaq_=identMaq;
         return true;
-     }
-     exit(0);
-     return false;
- }
+    }
+    return false;
+}
 
 
- void Maquina::darAlta_Baja_Maquina(){
+void Maquina::darAlta_Baja_Maquina(){
     remove("Maquinas.txt");
     fstream f("Maquinas.txt", fstream::app);
     if(!f){
@@ -32,9 +31,25 @@ bool Maquina::setMaquinasDisponibles(int identMaq){
     for(int i=0;i<nMaquinas_;i++){
         cin>>numMaquinas;
         setMaquinasDisponibles(numMaquinas);
-        f<<getMaquinasDisponibles()<<endl;
+        f<<"Maquina "<<getMaquinasDisponibles()<<endl;
         
     }
 
     f.close();
- }
+}
+
+void Maquina::mostrarDisponibilidad(){//Muestra el valor de las fechas ocupadas sin mostrar los usuarios que han realizado las reservas
+    FILE* f;
+    long medida;
+	char* texto;	
+    const char* nombre="Maquinas.txt";
+	f=fopen(nombre, "r");
+	fseek(f, 0, SEEK_END);
+    medida=ftell(f);
+    rewind(f);
+
+   	texto=(char*)malloc (sizeof(char)*medida);
+	fread(texto, medida+1, 1, f);
+    std::cout<<texto<<std::endl;
+    fclose(f);
+}
--- Maquinas.h
+++ Maquinas.h
@@ -7,16 +7,14 @@ class Maquina{
     private:
         int identMaq_;
         int nMaquinas_;
+        
     public:
-       inline Maquina(){};
+        inline Maquina(){};
         bool setnumMaquinas(int nMaquina);
         inline int getnumMaquinas(){return nMaquinas_;};
-
         bool setMaquinasDisponibles(int identMaq);
         inline int getMaquinasDisponibles(){return identMaq_;};
-        
         void darAlta_Baja_Maquina();
-        
+        void mostrarDisponibilidad();
 };
-
 #endif
